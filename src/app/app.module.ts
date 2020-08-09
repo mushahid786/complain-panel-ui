0ec0f9edd057cd1ed11c6341c61903d9b30fa1f7
Mushahid Khan <mushahid.khan@codeb.online>
2020-08-10 00:01:25 +0530

    form group added and logo added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsDatepickerModule, DatepickerModule } from "ngx-bootstrap/datepicker";
+import { NgxSelectModule } from 'ngx-select-ex';
 import { ComplainFormComponent } from './component/complain-form/complain-form.component';
 
 @NgModule({
@@ -12,7 +17,13 @@ import { ComplainFormComponent } from './component/complain-form/complain-form.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot(),
+    DatepickerModule.forRoot(),
+    NgxSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/complain-form/complain-form.component.ts
+++ src/app/component/complain-form/complain-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-complain-form',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComplainFormComponent implements OnInit {
 
-  constructor() { }
+
+  productsName = [
+    { id: "LG" },
+    { id: "Samsung" },
+    { id: "Westway" },
+    { id: "Haier" },
+    { id: "Warsi Cooler" },
+  ]
+
+  products = [
+    { id: "LED" },
+    { id: "Washing Machine" },
+    { id: "Refreigerator" },
+    { id: "Cooler Problem" }
+  ]
+
+  constructor(private fb: FormBuilder) { }
+
+  myForm: FormGroup;
+  bsConfigGlobal = {
+    dateInputFormat: "DD/MM/YYYY",
+    showWeekNumbers: false,
+    containerClass: "theme-default"
+  };
+  minDate: Date;
 
   ngOnInit(): void {
+
+    this.myForm = this.fb.group({
+      name: new FormControl(""),
+      phoneNumber: new FormControl(""),
+      email: new FormControl(""),
+      state: new FormControl(""),
+      district: new FormControl(""),
+      pinCode: new FormControl(""),
+      address: new FormControl(""),
+      productName: new FormControl(""),
+      product: new FormControl(""),
+      itemId: new FormControl(""),
+      modalNumber: new FormControl(""),
+      serialNumber: new FormControl(""),
+      description: new FormControl(""),
+      complainDate: new FormControl(""),
+
+    });
   }
 
+  selectedItem(value) {
+    console.log("Tesssssss", value);
+
+  }
 }
