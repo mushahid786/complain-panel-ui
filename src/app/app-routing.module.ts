553df4553eadad99335f5ae70f869b666058cedb
Mushahid Khan <mushahid.khan@codeb.online>
2020-08-09 14:54:41 +0530

    bootsrap added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComplainFormComponent } from './component/complain-form/complain-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: ComplainFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
